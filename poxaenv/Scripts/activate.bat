d76bc7d911c352624b792f0e5d3c0c468cf0f0bd
shaua <shauangel2000@gmail.com>
2024-09-24 17:12:05 +0800

    save

--- poxaenv/Scripts/activate.bat
+++ poxaenv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=D:\碩\POXA_chatbot\pdftest\POXA-backend-\poxaenv
+set VIRTUAL_ENV=C:\Users\shaua\Desktop\mine\poxagpt\POXA-backend-\poxaenv
 
 if not defined PROMPT set PROMPT=$P$G
 
