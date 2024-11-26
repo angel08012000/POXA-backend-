5583b2c81073e1bd01ea5d8d25290a68173865e0
Victoria.White <95166075+Smilevictoria@users.noreply.github.com>
2024-11-27 03:09:06 +0800

    Update main.py

--- main.py
+++ main.py
@@ -362,5 +362,6 @@ def chat_with_bot():
 if __name__ == '__main__':
     # app.run(host='0.0.0.0', debug=True)
     port = int(os.environ.get("PORT", 5000))  # Render
+    print(f"Starting app on port {port}")
     app.run(host="0.0.0.0", port=port)
 
